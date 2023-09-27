57e32f0c2c45a97d6e66f0ae1edf995aacbe0bf1
Tony Wang <Tony94042@gmail.com>
2023-09-27 15:23:43 +1000

    Started working on table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent {
-
+  data = [
+  { name: 'James', age: 24, job: 'Designer'},
+  { name: 'Bill', age: 34, job: 'QA'},
+  { name: 'Madison', age: 22, job: 'Developer'}
+  ];
+  headers = [
+  { key: 'Name', label: 'Name'},
+  { key: 'Age', label: 'Age'},
+  { key: 'Job', label: 'Job'}
+  ]
 }
