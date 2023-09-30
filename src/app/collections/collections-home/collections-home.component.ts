3d49055c48f94eface1559ef64e29a9b49ebddba
Tony Wang <Tony94042@gmail.com>
2023-09-30 12:37:12 +1000

    Finished the table module

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,15 +7,14 @@ import { Component } from '@angular/core';
 })
 export class CollectionsHomeComponent {
   data = [
-  { name: 'James', age: 24, job: 'Designer', employed: 'true'},
-  { name: 'Bill', age: 34, job: 'QA', employed: 'true'},
-  { name: 'Madison', age: 22, job: 'Developer', employed: 'true'}
+  { name: 'James', age: 24, job: 'Designer'},
+  { name: 'Bill', age: 34, job: 'QA'},
+  { name: 'Madison', age: 22, job: 'Developer'}
   ];
   headers = [
-  { key: 'Employed', label: true},
-  { key: 'Name', label: 'Name'},
-  { key: 'Age', label: 'Age'},
-  { key: 'Job', label: 'Job'}
+  { key: 'name', label: 'name'},
+  { key: 'age', label: 'age'},
+  { key: 'job', label: 'job'}
   ]
   // You always pass data from parent to child
 }
